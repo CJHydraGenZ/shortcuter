2a77a9c47ed2e0d724a11cd4c053dab219e63736
Cahya <cahyasaga321@gmail.com>
2022-08-13 08:03:49 +0800

    first package

---
+++ shortcuter.js
@@ -0,0 +1,14 @@
+// console.log('hellow world');
+
+
+
+
+
+export let RandomArray = (URandom) => URandom[Math.floor(Math.random() * URandom.length)]
+// export let randomArray = userAgent[Math.floor(Math.random() * userAgent.length)];
+// const data = [1, 2, 3, 4, 5, 6]
+
+// console.log(RandomArray(data));
+
+
+// export default RandomArray
